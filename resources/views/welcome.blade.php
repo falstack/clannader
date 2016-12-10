abf1eaebb30a786ee9065f6306986f3c3baedb9d
falstack <icesilt@outlook.com>
2016-12-10 21:38:23 +0800

    do something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,43 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <title>星火话</title>
-    <style>
-        html, body {
-            height: 100%;
-        }
-
-        body {
-            margin: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            font-size: 14px;
-            font-weight: 400;
-            background-color: #000000;
-            color: RGB(104, 253, 254);
-            font-family: -apple-system,Helvetica Neue,Arial,PingFang SC,Hiragino Sans GB,Microsoft YaHei,WenQuanYi Micro Hei,sans-serif;
-        }
-
-        .light {
-            animation: breath .75s linear infinite;
-        }
-
-        @keyframes breath
-        {
-            0% {
-                opacity: 0;
-            }
-            50% {
-                opacity: 1;
-            }
-            100% {
-                opacity: 0;
-            }
-        }
-    </style>
+    <link rel="shortcut icon" href="http://cdn.clannader.com/favicon.ico">
+    <meta id="_auth" content="{{ Auth::check() }}">
+    <title>CLANNADer - 天下漫友是一家</title>
 </head>
 <body>
-{ do something }<span class="light">&nbsp;_</span>
+<div id="app"></div>
+<script src="http://cdn.bootcss.com/jquery/3.1.0/jquery.min.js"></script>
+<script src="http://cdn.bootcss.com/vue/2.0.3/vue.min.js"></script>
+<script src="http://cdn.bootcss.com/vue-router/2.0.0/vue-router.min.js"></script>
+<script src="http://cdn.bootcss.com/vuex/2.0.0/vuex.min.js"></script>
+<script src="http://cdn.bootcss.com/vue-resource/1.0.3/vue-resource.min.js"></script>
+<script src="http://cdn.bootcss.com/socket.io/1.4.8/socket.io.min.js"></script>
+<script src="http://cdn.bootcss.com/lodash.js/4.16.2/lodash.min.js"></script>
+<script src="http://cdn.bootcss.com/plupload/2.1.9/plupload.full.min.js"></script>
+<script src="http://cdn.bootcss.com/qiniu-js/1.0.14-beta/qiniu.min.js"></script>
+<script src="http://cdn.bootcss.com/wangeditor/2.1.20/js/wangEditor.min.js"></script>
+<script src="{{ asset('dev/js/entry.js') }}"></script>
 </body>
 </html>
